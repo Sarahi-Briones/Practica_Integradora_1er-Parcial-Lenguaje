6b9619d933e4166d43b0a17ca2954382d8bc7d08
Sarahi-Briones <99497309+Sarahi-Briones@users.noreply.github.com>
2023-09-24 20:48:22 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import limpieza_texto
+
+# df = pd.read_excel('Rest_Mex_2022_Sentiment_Analysis_Track_Train.xlsx')
+# df['Opinion']= limpieza_texto.procesar_texto(df['Opinion'])
+
+# df.to_excel('archivo_procesado.xls', index=False, engine='openpyxl')
