8fe3f991af421e275700cf2f137a8d01897ec06f
Sarahi-Briones <99497309+Sarahi-Briones@users.noreply.github.com>
2023-10-03 20:33:25 -0600

    Update practica1_parte2.py

--- practica1_parte2.py
+++ practica1_parte2.py
@@ -5,7 +5,7 @@ import pandas as pd
 import spacy
 
 wordcloud = WordCloud(width=800, height=400, background_color='white', colormap='viridis', max_words=200)
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("es_core_news_sm")
 
 df = pd.read_excel('archivo_procesado.xls')
 opiniones_hotel = df[df['Attraction'] == 'Hotel']['Opinion'].tolist()
@@ -24,9 +24,22 @@ wordcloud.generate(text_hotel)
 # wordcloud.to_file('wordcloud_hotel.png')
 wordcloud_data_hotel = wordcloud.words_
 word_list_hotel = list(wordcloud_data_hotel.keys())
-#frequencies_hotel = list(wordcloud_data_hotel.values())
 doc_hotel = nlp(" ".join(word_list_hotel))
 etiquetas_pos_hotel = [(token.text, token.pos_) for token in doc_hotel]
+adjetivos_hotel = []
+verbos_hotel = []
+sustantivos_hotel =[]
+ner_hotel =[]
+for palabra, etiqueta_pos in etiquetas_pos_hotel:
+    if etiqueta_pos=='ADJ':
+        adjetivos_hotel.append(palabra)
+    elif etiqueta_pos=='VERB':
+        verbos_hotel.append(palabra)
+    elif etiqueta_pos=='NOUN':
+        sustantivos_hotel.append(palabra)
+    elif etiqueta_pos=='PROPN':
+        ner_hotel.append(palabra)
+
 
 # Generamos el WordCloud de las opiniones de la categoría Restaurante
 wordcloud_restaurante= wordcloud.generate(text_restaurante)
@@ -37,9 +50,22 @@ plt.figure(figsize=(10, 5))
 # wordcloud.to_file('wordcloud_restaurante.png')
 wordcloud_data_restaurante = wordcloud.words_
 word_list_restaurante = list(wordcloud_data_restaurante.keys())
-#frequencies_restaurante = list(wordcloud_data_restaurante.values())
 doc_restaurante = nlp(" ".join(word_list_restaurante))
 etiquetas_pos_restaurante = [(token.text, token.pos_) for token in doc_restaurante]
+adjetivos_restaurante = []
+verbos_restaurante = []
+sustantivos_restaurante =[]
+ner_restaurante =[]
+for palabra, etiqueta_pos in etiquetas_pos_restaurante:
+    if etiqueta_pos=='ADJ':
+        adjetivos_restaurante.append(palabra)
+    elif etiqueta_pos=='VERB':
+        verbos_restaurante.append(palabra)
+    elif etiqueta_pos=='NOUN':
+        sustantivos_restaurante.append(palabra)
+    elif etiqueta_pos=='PROPN':
+        ner_restaurante.append(palabra)
+        
 
 # Generamos el WordCloud de las opiniones de la categoría Atracción
 wordcloud_atraccion= wordcloud.generate(text_atraccion)
@@ -50,8 +76,19 @@ wordcloud_atraccion= wordcloud.generate(text_atraccion)
 # wordcloud.to_file('wordcloud_atraccion.png')
 wordcloud_data_atraccion = wordcloud.words_
 word_list_atraccion = list(wordcloud_data_atraccion.keys())
-#frequencies_atraccion = list(wordcloud_data_atraccion.values())
 doc_atraccion = nlp(" ".join(word_list_atraccion))
 etiquetas_pos_atraccion = [(token.text, token.pos_) for token in doc_atraccion]
-
-print(etiquetas_pos_hotel)
+adjetivos_atraccion = []
+verbos_atraccion = []
+sustantivos_atraccion =[]
+ner_atraccion =[]
+for palabra, etiqueta_pos in etiquetas_pos_atraccion:
+    if etiqueta_pos=='ADJ':
+        adjetivos_atraccion.append(palabra)
+    elif etiqueta_pos=='VERB':
+        verbos_atraccion.append(palabra)
+    elif etiqueta_pos=='NOUN':
+        sustantivos_atraccion.append(palabra)
+    elif etiqueta_pos=='PROPN':
+        ner_atraccion.append(palabra)
+        
