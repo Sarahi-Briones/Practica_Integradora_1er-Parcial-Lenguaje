9733ce51980544357340ae54b80159940f64769f
Sarahi-Briones <99497309+Sarahi-Briones@users.noreply.github.com>
2023-10-03 19:54:35 -0600

    Update practica1_parte2.py

--- practica1_parte2.py
+++ practica1_parte2.py
@@ -2,17 +2,12 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import numpy as np
 import pandas as pd
+import spacy
 
-wordcloud = WordCloud(
-    width=800,
-    height=400,
-    background_color='white',
-    colormap='viridis',  # Cambia el colormap según tus preferencias de color
-    max_words=200       # Número máximo de palabras a mostrar en el Word Cloud
-)
+wordcloud = WordCloud(width=800, height=400, background_color='white', colormap='viridis', max_words=200)
+nlp = spacy.load("en_core_web_sm")
 
 df = pd.read_excel('archivo_procesado.xls')
-
 opiniones_hotel = df[df['Attraction'] == 'Hotel']['Opinion'].tolist()
 opiniones_restaurante = df[df['Attraction'] == 'Restaurant']['Opinion'].tolist()
 opiniones_atraccion = df[df['Attraction'] == 'Attractive']['Opinion'].tolist()
@@ -22,24 +17,41 @@ text_atraccion = ' '.join(opiniones_atraccion)
 
 # Generamos el WordCloud de las opiniones de la categoría Hotel
 wordcloud.generate(text_hotel)
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')  # Oculta los ejes
-plt.show()
-wordcloud.to_file('wordcloud_hotel.png')
+# plt.figure(figsize=(10, 5))
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis('off')  # Oculta los ejes
+# plt.show()
+# wordcloud.to_file('wordcloud_hotel.png')
+wordcloud_data_hotel = wordcloud.words_
+word_list_hotel = list(wordcloud_data_hotel.keys())
+#frequencies_hotel = list(wordcloud_data_hotel.values())
+doc_hotel = nlp(" ".join(word_list_hotel))
+etiquetas_pos_hotel = [(token.text, token.pos_) for token in doc_hotel]
 
 # Generamos el WordCloud de las opiniones de la categoría Restaurante
 wordcloud_restaurante= wordcloud.generate(text_restaurante)
 plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')  # Oculta los ejes
-plt.show()
-wordcloud.to_file('wordcloud_restaurante.png')
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis('off')  # Oculta los ejes
+# plt.show()
+# wordcloud.to_file('wordcloud_restaurante.png')
+wordcloud_data_restaurante = wordcloud.words_
+word_list_restaurante = list(wordcloud_data_restaurante.keys())
+#frequencies_restaurante = list(wordcloud_data_restaurante.values())
+doc_restaurante = nlp(" ".join(word_list_restaurante))
+etiquetas_pos_restaurante = [(token.text, token.pos_) for token in doc_restaurante]
 
 # Generamos el WordCloud de las opiniones de la categoría Atracción
 wordcloud_atraccion= wordcloud.generate(text_atraccion)
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')  # Oculta los ejes
-plt.show()
-wordcloud.to_file('wordcloud_atraccion.png')
+# plt.figure(figsize=(10, 5))
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis('off')  # Oculta los ejes
+# plt.show()
+# wordcloud.to_file('wordcloud_atraccion.png')
+wordcloud_data_atraccion = wordcloud.words_
+word_list_atraccion = list(wordcloud_data_atraccion.keys())
+#frequencies_atraccion = list(wordcloud_data_atraccion.values())
+doc_atraccion = nlp(" ".join(word_list_atraccion))
+etiquetas_pos_atraccion = [(token.text, token.pos_) for token in doc_atraccion]
+
+print(etiquetas_pos_hotel)
